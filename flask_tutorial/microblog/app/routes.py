6115f75b21c098bbbad4b6bf2c6d932a108747ee
Matic Lubej <matic.lubej@sinergise.com>
2020-03-09 16:59:19 +0100

    add tutorial

---
+++ flask_tutorial/microblog/app/routes.py
@@ -0,0 +1,15 @@
+from app import app
+
+@app.route('/')
+@app.route('/index')
+def index():
+    user = {'username': 'Matic'}
+    return '''
+<html>
+    <head>
+        <title>Home Page - Microblog</title>
+    </head>
+    <body>
+        <h1>Hello, ''' + user['username'] + '''!</h1>
+    </body>
+</html>'''
