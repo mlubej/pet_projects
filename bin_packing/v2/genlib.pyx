a73a4516021df80fa0f00c5a49d753d2a3acf252
Matic Lubej <matic.lubej@sinergise.com>
2020-04-08 09:58:44 +0200

    updating things

--- bin_packing/v2/genlib.pyx
+++ bin_packing/v2/genlib.pyx
@@ -183,7 +183,7 @@ class Individual(object):
 
 
 class Evolution(object):
-    def __init__(self, npop, npcs=len(polygons), mut_threshold=0.01, instance=0):
+    def __init__(self, npop, npcs=len(polygons), mut_threshold=0.01, instance=0, fdir='./'):
         self.npop = npop + 1 if npop % 2 != 0 else npop
         self.npcs = npcs
         self.mut_threshold = mut_threshold
@@ -192,6 +192,7 @@ class Evolution(object):
         self.generation = 0
         self.instance = instance
         self.history = []
+        self.fdir = fdir
 
     def initialize_population(self):
         self.population = [Individual() for i in range(self.npop)]
@@ -228,27 +229,31 @@ class Evolution(object):
         best_id = np.argsort([ind.fitness for ind in self.population])[0]
         return self.population[best_id]
 
+    def plot_process(self, fname=''):
+        if fname == '':
+            plot_process(np.array(self.history))
+        else:
+            plot_process(np.array(self.history), f'{self.fdir}/plot_process_{self.instance}.png')
+
+
     def darwinize(self):
         print('Initializing')
         self.initialize_population()
         self.mutate_generation()
         self.best = self.get_best_candidate()
         self.history.append([self.generation, self.best.fitness])
-        self.best.plot(f'./plot_puzzle_{self.instance}.png')
-        plot_process(np.array(self.history), f'./plot_process_{self.instance}.png')
+        self.best.plot(f'{self.fdir}/plot_sol_{self.instance}.png')
+        self.plot_process(f'{self.fdir}/plot_proc_{self.instance}.png')
         print(
             f'Generation: {self.generation}, best candidate score: {self.best.fitness}, chromosome: [{",".join(np.array(self.best.chromosome, dtype=str))}]')
-        try:
-            while not self.check_condition():
-                print('Creating next generation')
-                self.next_generation()
-                best = self.get_best_candidate()
-                if best.fitness < self.best.fitness:
-                    self.best = best
-                    self.history.append([self.generation, self.best.fitness])
-                    self.best.plot(f'./plot_puzzle_{self.instance}.png')
-                    plot_process(np.array(self.history), f'./plot_process_{self.instance}.png')
-                    print(
-                        f'Generation: {self.generation}, best candidate score: {self.best.fitness}, chromosome: [{",".join(np.array(self.best.chromosome, dtype=str))}]')
-        except KeyboardInterrupt:
-            print('Interrupting!')
+        while not self.check_condition():
+            print('Creating next generation')
+            self.next_generation()
+            best = self.get_best_candidate()
+            if best.fitness < self.best.fitness:
+                self.best = best
+                self.history.append([self.generation, self.best.fitness])
+                self.best.plot(f'{self.fdir}/plot_sol_{self.instance}.png')
+                self.plot_process(f'{self.fdir}/plot_proc_{self.instance}.png')
+                print(
+                    f'Generation: {self.generation}, best candidate score: {self.best.fitness}, chromosome: [{",".join(np.array(self.best.chromosome, dtype=str))}]')
